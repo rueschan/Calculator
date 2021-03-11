0eb1196be9ded8c7072d88f7136e90234c4f8575
Rubén Escalante <rescalante21@hotmail.com>
2021-03-11 17:13:11 -0600

    Feature: Front files added (HTML & CSS)

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 320,
+    height: 600,
+    // resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, './src/preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  window.loadFile('./public/html/index.html');
+  window.webContents.openDevTools(); // DEBUG-ONLY
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
